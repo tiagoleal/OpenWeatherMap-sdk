c8e1acca9b95c7b797a0e0167f927132d5ce2b0c
Tiago Leal <tiago.sleal12@gmail.com>
2021-01-14 23:38:22 -0200

    created feature to return next five weather forecast

--- lib/OpenWeatherMap.rb
+++ lib/OpenWeatherMap.rb
@@ -4,7 +4,6 @@ require 'OpenWeatherMap/version'
 
 # Dependencia da gem que faz a chamada REST
 require 'rest-client'
-
 # Dependencia para parsear o JSON
 require 'json'
 
@@ -25,7 +24,8 @@ module OpenWeatherMap
     def get_openweather_temperature
       Time.zone = 'America/Sao_Paulo'
       current_weather = request_current_weather
-      current_weather
+      next_weather_forecast = request_next_weather_forecast
+      return send_weather_forecast(current_weather, next_weather_forecast)
     end
 
     private
@@ -39,6 +39,37 @@ module OpenWeatherMap
         description: parse_current_weather['weather'][0]['description'],
         city: parse_current_weather['name']
       }
+    end
+
+    def request_next_weather_forecast
+      forecast_weather = "http://api.openweathermap.org/data/2.5/forecast?q=Santa%20Cruz%20do%20Sul&lang=pt_br&units=metric&appid=#{@api_key}"
+      parse_forecast_weather = request_openweather(forecast_weather)
+
+      timestamp_now = Time.now.to_i
+      weather_forecast = parse_forecast_weather['list'].map do |temp|
+        next unless timestamp_now <= temp['dt']
+
+        info_temperature = {
+          timestamp: temp['dt'],
+          dt: Time.zone.at(temp['dt']).strftime('%d/%m/%Y %H:%M:%I'),
+          graus: (temp['main']['temp']).to_s,
+          clima: (temp['weather'][0]['description']).to_s,
+          date: Time.zone.at(temp['dt']).strftime('%d/%m')
+        }
+      end
+      weather_forecast.group_by { |h| h[:date] }.transform_values { |hs| hs.map { |h| h[:graus].to_i } }
+    end
+
+    def send_weather_forecast(current_weather, next_weather_forecast)
+      message = "#{current_weather[:temp]}°C e #{current_weather[:description]} em #{current_weather[:city]} "
+      message += "em #{current_weather[:date]}. Média para os próximos dias: "
+
+      next_weather_forecast.each do |day, temp|
+        next if current_weather[:date].to_s == day.to_s
+        average = temp.reduce(:+) / temp.count
+        message += "#{average}°C em #{day}, "
+      end
+      { message: message }
     end
 
     def request_openweather(url)
